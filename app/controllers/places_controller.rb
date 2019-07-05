23d53f8478e85957d78a0c67fb35cbd3508b48e4
Michael <mr.enviro@gmail.com>
2019-07-05 04:51:04 +0000

    Connect Users to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+
 
 	def index
 		@places = Place.all.paginate(:page => params[:page], :per_page => 5)
@@ -9,11 +11,13 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
 
+	
 	private
+
 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
