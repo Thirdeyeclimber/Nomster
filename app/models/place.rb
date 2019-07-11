925dabdaf469b47a093a346ec2a5fb7ca1405afc
Michael <mr.enviro@gmail.com>
2019-07-11 17:57:03 +0000

    Add and connect 'Comments' database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,11 @@
 class Place < ApplicationRecord
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
 	after_validation :geocode
 	
-	validates :address, :description, presence: true
 	validates :name, length: { minimum: 2,
     too_short: "%{count} characters is the minimum allowed" }, presence: true
+  validates :address, :description, presence: true
 end
