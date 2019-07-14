5606380f3837a94f6b92f7644ea542f47529c3f3
Michael <mr.enviro@gmail.com>
2019-07-14 21:06:36 +0000

    working on jquery issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+//= require jquery_ujs
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -18,7 +22,5 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-//= require jquery
-//= require jquery_ujs
 //= require popper
 //= require bootstrap
